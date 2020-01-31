ee8c5adcd26c44a02f88a578af907b9af90e2664
Raymond Schade <ray@kacluc.lu>
2020-01-31 17:57:03 -0500

    this kid is cleaning up

--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -1,6 +1,10 @@
 import math
 import agent
 
+
+import sys
+
+
 ###########################
 # Alpha-Beta Search Agent #
 ###########################
@@ -24,8 +28,175 @@ class AlphaBetaAgent(agent.Agent):
     #
     # NOTE: make sure the column is legal, or you'll lose the game.
     def go(self, brd):
-        """Search for the best move (choice of column for the token)"""
-        # Your code here
+        (v, drop) = self.alpha_beta((brd, 0), 3, -math.inf, math.inf, True)
+        print("VALUE", v, "MOVE", drop)
+        return drop
+
+    def alpha_beta(self, brd_tuple, depth, alpha, beta, is_max_player):
+        brd = brd_tuple[0]
+        outcome = brd.get_outcome()
+        v = math.nan
+        col = math.nan
+        if depth == 0 or outcome != 0:
+            if outcome == 1:
+                return (-math.inf, brd_tuple[1])
+            elif outcome == 2:
+                return (math.inf, brd_tuple[1])
+            return self.heuristic(brd_tuple)
+        if is_max_player:
+            v = -math.inf
+            for child in self.get_successors(brd):
+                (n_v, col) = self.alpha_beta(child, depth - 1, alpha, beta, False)
+                v = max(v, n_v)
+                alpha = max(alpha, v)
+                if alpha >= beta:
+                    break
+        else:
+            v = math.inf
+            for child in self.get_successors(brd):
+                (n_v, col) = self.alpha_beta(child, depth - 1, alpha, beta, True)
+                v = min(v, n_v)
+                beta = min(beta, v)
+                if alpha >= beta:
+                    break 
+        return (v, col)
+
+    def playable_chain(self, brd, target):
+        y = -1
+        longest_chain = 0
+        longest_playable_chain = 0
+        zero_counter = 0
+        for row in brd.board:
+            y = y + 1
+            if (y >= brd.h):
+                break
+            zero_counter = 0
+            for x in range(len(row)):
+                #Increment blank row counter and check for max blank row
+                if (x >= brd.w):
+                    break
+                if brd.board[y][x] == 0:
+                    zero_counter = zero_counter + 1
+                    if zero_counter >= brd.w:
+                        break
+                if target == brd.board[y][x]:
+                    start_x = x
+                    start_y = y
+                    # Check up
+                    longest_chain = 0
+                    while brd.board[y][x] == target:
+                        longest_chain = longest_chain + 1
+                        y = y + 1
+                        if (y >= brd.h or y < 0 or x >= brd.w or x < 0):
+                            break
+                        if (y - 1 < 0):
+                            if brd.board[y][x] == 0:
+                                if longest_chain > longest_playable_chain:
+                                    longest_playable_chain = longest_chain
+                                    break
+                        else:
+                            if brd.board[y][x] == 0 and brd.board[y-1][x] != 0:
+                                if longest_chain > longest_playable_chain:
+                                    longest_playable_chain = longest_chain
+                                    break
+                    # Reset to curr token
+                    x = start_x
+                    y = start_y
+                    longest_chain = 0
+                    # Check right
+                    while brd.board[y][x] == target:
+                        longest_chain = longest_chain + 1
+                        x = x + 1
+                        if (y >= brd.h or y < 0 or x >= brd.w or x < 0):
+                            break
+                        if (y - 1 < 0):
+                            if brd.board[y][x] == 0:
+                                if longest_chain > longest_playable_chain:
+                                    longest_playable_chain = longest_chain
+                                    break
+                        else:
+                            if brd.board[y][x] == 0 and brd.board[y-1][x] != 0:
+                                if longest_chain > longest_playable_chain:
+                                    longest_playable_chain = longest_chain
+                                    break
+                    # Reset to curr token
+                    x = start_x
+                    y = start_y
+                    longest_chain = 0
+                    # Check diag right
+                    while brd.board[y][x] == target:
+                        longest_chain = longest_chain + 1
+                        x = x + 1
+                        y = y + 1
+                        if (y >= brd.h or y < 0 or x >= brd.w or x < 0):
+                            break
+                        if (y - 1 < 0):
+                            if brd.board[y][x] == 0:
+                                if longest_chain > longest_playable_chain:
+                                    longest_playable_chain = longest_chain
+                                    break
+                        else:
+                            if brd.board[y][x] == 0 and brd.board[y-1][x] != 0:
+                                if longest_chain > longest_playable_chain:
+                                    longest_playable_chain = longest_chain
+                                    break
+                    # Reset to curr token
+                    x = start_x
+                    y = start_y
+                    longest_chain = 0
+                    # Check diag left
+                    while brd.board[y][x] == target:
+                        longest_chain = longest_chain + 1
+                        x = x - 1
+                        y = y + 1
+                        if (y >= brd.h or y < 0 or x >= brd.w or x < 0):
+                            break
+                        if (y - 1 < 0):
+                            if brd.board[y][x] == 0:
+                                if longest_chain > longest_playable_chain:
+                                    longest_playable_chain = longest_chain
+                                    break
+                        else:
+                            if brd.board[y][x] == 0 and brd.board[y-1][x] != 0:
+                                if longest_chain > longest_playable_chain:
+                                    longest_playable_chain = longest_chain
+                                    break
+                    x = start_x
+                    y = start_y
+                    longest_chain = 0
+                    # Check left
+                    while brd.board[y][x] == target:
+                        x = x - 1
+                        longest_chain = longest_chain + 1
+                        if (y >= brd.h or y < 0 or x >= brd.w or x < 0):
+                            break
+                        if (y - 1 < 0):
+                            if brd.board[y][x] == 0:
+                                if longest_chain > longest_playable_chain:
+                                    longest_playable_chain = longest_chain
+                                    break
+                        else:
+                            if brd.board[y][x] == 0 and brd.board[y-1][x] != 0:
+                                if longest_chain > longest_playable_chain:
+                                    longest_playable_chain = longest_chain
+                                    break
+
+        return longest_playable_chain
+
+    def heuristic(self, brd_tuple):
+        # brd_tuple is (board state, column where last token was added)
+        # return either an approximation of the value of moving to this board state
+        # -inf if this will cause the minimizing player to win and
+        # inf if the maximizing player will win
+        olpc = self.playable_chain(brd_tuple[0], 2)
+        xlpc = self.playable_chain(brd_tuple[0], 1)
+        h = olpc - (3*xlpc)
+        print("CHAIN FOR 1:", self.playable_chain(brd_tuple[0], 1),
+        "CHAIN FOR 2:" , self.playable_chain(brd_tuple[0], 2))
+        brd_tuple[0].print_it()
+        print(h, brd_tuple[1])
+        print("----")
+        return (h, brd_tuple[1])
 
     # Get the successors of the given board.
     #
