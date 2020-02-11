c7a097f371b60810ed8dd1d32ad52c9352392503
Raymond Schade <ray@kacluc.lu>
2020-02-10 20:43:32 -0500

    optimized for most amount of almost winning chains

--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -40,6 +40,7 @@ class AlphaBetaAgent(agent.Agent):
         else:
             self.max_depth = 9
         (v, move) = self.alpha_beta((brd, 0), self.max_depth, -math.inf, math.inf, True)
+        print(len(self.trans_table))
         print("DEPTH", self.max_depth, "VALUE", v, "MOVE", move)
         return move
     
@@ -86,116 +87,6 @@ class AlphaBetaAgent(agent.Agent):
                 if alpha >= beta:
                     break
         return (v, col)
-
-    def playable_chain_single(self, brd, row, col, drow, dcol, target):
-        '''
-        @author Dillon (just working through this)
-        brd as game board
-        row as root row coordinate
-        col as root column coordinate
-        drow as delta row
-        dcol as delta col
-        target as 1 or 2 denoting player
-        returns the length of the chain
-        '''
-        # set r and c pre-traversal
-        #r, c = row + drow, col + dcol
-        r, c = row + drow, col + dcol
-        # store length of playable chain
-        length = 0 #FIXME should this start at 0?
-        # verify that row,col cursor is still on the board
-        while r in range(0, brd.h) and c in range(0, brd.w):
-            # print(r, c, brd.board[r][c])
-            # verify that r,c cursor is on target
-            if brd.board[r][c] != target:
-                #print('no') # NOTE
-                return length
-            # increment chain length
-            length += 1
-            # return n if length is equal to n
-            if length == brd.n:
-                return length
-            # apply row and column deltas
-            r += drow
-            c += dcol
-        # we went off the board, give what we have
-        return length
-
-    def playable_chain(self, brd, target):
-        '''@author Dillon
-        brd as game board
-        target as 1 or 2 for player
-        returns the longest! playable chain for a target
-        '''
-        
-        # keep all chains
-        chains = []
-        # columns to look through (not found to be empty)
-        cols = list(range(0, brd.w))
-        new_cols = cols
-        # iterate row first
-        for r in range(0, brd.h):
-            if len(new_cols) == 0:
-                break
-            new_cols = []
-            for c in cols:
-                if brd.board[r][c] != 0:
-                    new_cols.append(c)
-                    continue
-                # define directions
-                directions = [
-                        (1, 0), # north
-                        (1, 1), # northeast
-                        (0, 1), # east
-                        (-1, 1), # southeast
-                        (-1, 0), # south
-                        (-1, -1), # southwest
-                        (0, -1), # west
-                        (1, -1)] # northwest
-                # iterate through directions
-                for drow, dcol in directions:
-                    # find chain length in this direction
-                    length = self.playable_chain_single(brd, r, c, drow, dcol, target)
-                    # print('(%d,%d) (%d,%d), target %d -> %d' % (c, r, dcol, drow, target, length))
-                    # if there is a chain, put it into the array
-                    if length > 0:
-                        chains.append(length)
-        # all the chains found
-        return chains
-        """
-        # keep all chains
-        chains = []
-        # iterate across columns
-        for c in range(0, brd.w):
-            # assume playable cell at 0,c
-            r = 0
-            # find the playable, empty cell in this column
-            while r in range(0, brd.h) and brd.board[r][c] != 0:
-                r += 1
-            # continue through column if height was reached
-            if r == brd.h:
-                continue
-            # define directions NOTE look into necessary directions
-            directions = [
-                    (0, 1), # north
-                    (1, 1), # northeast
-                    (1, 0), # east
-                    (1, -1), # southeast
-                    (0, -1), # south
-                    (-1, -1), # southwest
-                    (-1, 0), # west
-                    (-1, 1)] # northwest
-            # iterate through directions
-            for drow, dcol in directions:
-                # find chain length in this direction
-                length = self.playable_chain_single(brd, r, c, drow, dcol, target)
-                # print('(%d,%d) (%d,%d), target %d -> %d' % (c, r, dcol, drow, target, length))
-                # if there is a chain, put it into the array
-                if length > 0:
-                    chains.append(length)
-        # all the chains found
-        return chains
-        """
     
     def check_path(self, brd, dr, dc, r, c, target):
         board = brd.board
@@ -259,20 +150,7 @@ class AlphaBetaAgent(agent.Agent):
                 cols = new_cols
         return winning_moves
 
-    def pc_weighted(self, brd, target):
-        """
-        chains = sorted(self.playable_chain(brd, target), reverse=True)
-        score = 0
-        # if there are two directly winning positions, then the game is over
-        if len(chains) >= 2 and chains[0] == chains[1] and chains[0] == brd.n - 1:
-            return math.inf
-        # othwerwise return the number of chains that are about to win
-        for chain in chains:
-            if chain < brd.n - 1:
-                break
-            score += chain
-        return score
-        """
+    def heuristic_target(self, brd, target):
         score = self.winning_moves(brd, target)
         # if there are two directly winning positions, then the game is over
         if score >= 2:
@@ -294,8 +172,8 @@ class AlphaBetaAgent(agent.Agent):
         # return either an approximation of the value of moving to this board state
         # -inf if this will cause the minimizing player to win and
         # inf if the maximizing player will win
-        olpc = self.pc_weighted(brd_tuple[0], self.max_player)
-        xlpc = self.pc_weighted(brd_tuple[0], self.min_player)
+        olpc = self.heuristic_target(brd_tuple[0], self.max_player)
+        xlpc = self.heuristic_target(brd_tuple[0], self.min_player)
         free = self.free_pos(brd_tuple[0])
         h = olpc - xlpc + (free / (10**len(str(free))))
         """
