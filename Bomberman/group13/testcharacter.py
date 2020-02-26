5716c480bd67ab198658189a7118e75264150402
Raymond Schade <ray@kacluc.lu>
2020-02-26 14:18:04 -0500

    maybe q learning

--- Bomberman/group13/testcharacter.py
+++ Bomberman/group13/testcharacter.py
@@ -4,8 +4,17 @@ sys.path.insert(0, '../bomberman')
 # Import necessary stuff
 from entity import CharacterEntity
 from colorama import Fore, Back
+from random import uniform, randrange
 
 class TestCharacter(CharacterEntity):
+
+    def __init__(self):
+        self.weights = None
+        self.gamma = 0.9
+        self.alpha = 1
+        self.q = 0
+        self.r = -0.01
+        self.eps = 1
 
     def do(self, wrld):
         pass
@@ -24,11 +33,49 @@ class TestCharacter(CharacterEntity):
         for the position using a hueristic or whatever.
         '''
         pass
+
+    def __calc_q(self, pair, weights):
+        '''
+        Calculates and returns the q value and weights
+        given a tuple pair for move and the current weights
+        '''
+        # retrieve state values and calculate new weights
+        state_vals = self.__get_value(pair)
+        if weights is None:
+            weights = [1 for _ in state_vals]
+        max_a = 0 # TODO: how do we calculate this?
+        delta = (self.r + self.gamma * max_a) - self.q
+        weights = [weights[i] + self.alpha * delta * f 
+            for i, f in enumerate(state_vals)]
+        # calculate the next q value step using approximate q learning
+        return (
+            sum([weights[i] * f for i, f in enumerate(state_vals)]),
+            weights
+        )
         
-    def __q_function(self, pair):
+    def __calc_next_move(self, pair):
         '''
-        Q function as defined by wikipedia (probably), use self
-        to determine current position, take tuple pair for move.
-        Note: if pair == current position, a bomb is to be dropped
+        Calculates the next move based on approximate q learning
         '''
-        pass
+        (self.q, self.weights) = self.__calc_q(pair, self.weights)
+        # take a new move using epsilon greedy exploration
+        new_move = None
+        next_moves = self.__list_next_moves()
+        x = uniform(0, 1)
+        if x < self.eps:
+            # exploration
+            new_move = next_moves[randrange(0,len(next_moves))]
+        else:
+            # exploitation
+            max_q = 0
+            for move in next_moves:
+                (cur_q, _) = self.__calc_q(move, self.weights)
+                if cur_q > max_q:
+                    max_q = cur_q
+                    new_move = move
+        # decrease epsilon
+        if self.eps > 0:
+            self.eps -= 0.01
+        if self.eps < 0:
+            self.eps = 0
+        return new_move
