4cfc275452a6dfef16387d9b1da6bb51ddb48150
Dillon Bordeleau <dillontbordeleau@gmail.com>
2020-02-26 13:35:32 -0500

    add function defs

--- Bomberman/group13/testcharacter.py
+++ Bomberman/group13/testcharacter.py
@@ -8,5 +8,27 @@ from colorama import Fore, Back
 class TestCharacter(CharacterEntity):
 
     def do(self, wrld):
-        # Your code here
+        pass
+        
+    def __list_next_moves(self):
+        '''
+        Use self to determine position on board, return
+        a list of coordinates representing legal next moves.
+        Note: return tuples
+        '''
+        pass
+        
+    def __get_value(self, pair):
+        '''
+        Use coordinate pair on board as given, get a value
+        for the position using a hueristic or whatever.
+        '''
+        pass
+        
+    def __q_function(self, pair):
+        '''
+        Q function as defined by wikipedia (probably), use self
+        to determine current position, take tuple pair for move.
+        Note: if pair == current position, a bomb is to be dropped
+        '''
         pass
